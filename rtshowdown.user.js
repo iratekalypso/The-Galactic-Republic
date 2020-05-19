15433fa36bdc909ca66be52e613ede7653aae576
iratekalypso <64004674+iratekalypso@users.noreply.github.com>
2020-05-19 10:23:31 -0400

    Update rtshowdown.user.js

--- rtshowdown.user.js
+++ rtshowdown.user.js
@@ -5,7 +5,7 @@
 // @description  Stop RT from their shenagigans and have ALL STAR WARS be in the final 4!
 // @author       Mission Vao
 // @match        https://editorial.rottentomatoes.com/article/summer-movie-showdown/
-// @updateurl    https://github.com/iratekalypso
+// @updateurl    https://github.com/iratekalypso/The-Galactic-Republic/raw/master/rtshowdown.user.js
 // ==/UserScript==
 window.addEventListener('load', function () {
   alert("It's loaded! VOTE VOTE VOTE")

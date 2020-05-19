89591b76420576955e2b90bd0719295f269ed3c4
iratekalypso <64004674+iratekalypso@users.noreply.github.com>
2020-05-19 16:26:51 -0400

    Update rtshowdown.user.js

--- rtshowdown.user.js
+++ rtshowdown.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         THE GALACTIC REPUBLIC
 // @namespace    https://discord.gg/thegalacticrepublic
-// @version      2.1
+// @version      2.5
 // @description  Stop RT from their shenagigans and have ALL STAR WARS be in the final 4!
 // @author       kalypso, AquilaeAnon
 // @match        https://editorial.rottentomatoes.com/article/summer-movie-showdown/
@@ -27,6 +27,14 @@ setInterval(function(){
     try{
 let iframe = document.getElementsByClassName('articleContentBody').item(0).getElementsByTagName('iframe').item(0);
 if(iframe !== null) {let vote_btn = iframe.contentWindow.document.getElementsByTagName("button")[2];
+                    vote_btn.click();}
+}catch(e){alert(e)}
+}, 8000);
+
+setInterval(function(){
+    try{
+let iframe = document.getElementsByClassName('articleContentBody').item(0).getElementsByTagName('iframe').item(0);
+if(iframe !== null) {let vote_btn = iframe.contentWindow.document.getElementsByTagName("button")[0];
                     vote_btn.click();}
 }catch(e){alert(e)}
 }, 8000);

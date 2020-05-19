de5ae1e5f73e0feeebdc9288b36c40f44bbd3be1
iratekalypso <64004674+iratekalypso@users.noreply.github.com>
2020-05-19 14:06:24 -0400

    Update rtshowdown.user.js
    Auto Click now,

--- rtshowdown.user.js
+++ rtshowdown.user.js
@@ -1,14 +1,35 @@
 // ==UserScript==
 // @name         THE GALACTIC REPUBLIC
 // @namespace    https://discord.gg/thegalacticrepublic
-// @version      1.3
+// @version      2.0
 // @description  Stop RT from their shenagigans and have ALL STAR WARS be in the final 4!
-// @author       kalypso
+// @author       kalypso AquilaeAnon
 // @match        https://editorial.rottentomatoes.com/article/summer-movie-showdown/
 // @updateurl    https://github.com/iratekalypso/The-Galactic-Republic/raw/master/rtshowdown.user.js
 // ==/UserScript==
-window.addEventListener('load', function () {
-  alert("It's loaded! VOTE VOTE VOTE")
-})
+setInterval(function(){
+    try{
+let iframe = document.getElementsByClassName('articleContentBody').item(0).getElementsByTagName('iframe').item(0);
+if(iframe !== null) {let vote_btn = iframe.contentWindow.document.getElementsByTagName("button")[6];
+                    vote_btn.click();}
+}catch(e){alert(e)}
+}, 8000);
+
+setInterval(function(){
+    try{
+let iframe = document.getElementsByClassName('articleContentBody').item(0).getElementsByTagName('iframe').item(0);
+if(iframe !== null) {let vote_btn = iframe.contentWindow.document.getElementsByTagName("button")[4];
+                    vote_btn.click();}
+}catch(e){alert(e)}
+}, 8000);
+
+setInterval(function(){
+    try{
+let iframe = document.getElementsByClassName('articleContentBody').item(0).getElementsByTagName('iframe').item(0);
+if(iframe !== null) {let vote_btn = iframe.contentWindow.document.getElementsByTagName("button")[2];
+                    vote_btn.click();}
+}catch(e){alert(e)}
+}, 8000);
+
 localStorage.clear();
-setTimeout(function(){ location.reload(); }, 15*1000);
+setTimeout(function(){ location.reload(); }, 10*1000);
